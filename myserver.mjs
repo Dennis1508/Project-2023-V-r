ce3129ac45a3c91e53f2994747ac6af793f8c51c
LAPTOP-AL2LA1UJ\Dennis An Vo <Dennisanvo@gmail.com>
2023-03-07 13:56:18 +0100

    commit

--- myserver.mjs
+++ myserver.mjs
@@ -3,17 +3,27 @@ const server = express();
 
 const port = (process.env.PORT || 8080)
 
+
+function what(req, res, next){
+    console.log(req, url);
+    next();
+}
+
+
+
 server.set('port', port);
 
 server.use(express.static('public'));
 
+server.use(what);
 
 
 
-
-
-
-
+server.get("/", (req, res,next) => {
+    console.log("Get /");
+    res.status(200).send("Hello World").end();
+    next();
+});
 
 
 
