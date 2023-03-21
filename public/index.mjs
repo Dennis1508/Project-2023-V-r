da9bada748282fa86dab9ee18db85d03d7666f62
LAPTOP-AL2LA1UJ\Dennis An Vo <Dennisanvo@gmail.com>
2023-03-21 13:39:29 +0100

    commit

--- public/index.mjs
+++ public/index.mjs
@@ -12,10 +12,19 @@ let addUserBtn = document.getElementById("addUserBtn");
 
 
 buttonEn.addEventListener("click",function(evt){
+headers.innerHTML = Dictionary.en.headers;
+buttonEn.innerHTML = Dictionary.en.buttonEn;
+buttonNo.innerHTML = Dictionary.en.buttonNo;
+txtBox.innerHTML = Dictionary.en.txtBox;
+
 
 });
 
 buttonNo.addEventListener("click",function(evt){
+headers.innerHTML = Dictionary.no.headers;
+buttonNo.innerHTML = Dictionary.no.buttonNo;
+buttonEn.innerHTML = Dictionary.no.buttonEn;
+txtBox.innerHTML = Dictionary.no.txtBox;
 
 });
 
