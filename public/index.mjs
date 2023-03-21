6d881f83250d26e976dfa45e89b24ce23c7a55b4
LAPTOP-AL2LA1UJ\Dennis An Vo <Dennisanvo@gmail.com>
2023-03-21 13:36:12 +0100

    commit

--- public/index.mjs
+++ public/index.mjs
@@ -3,12 +3,21 @@ import Dictionary from "./Dictionary.mjs";
 
 let buttonEn = document.getElementById("buttonEn");
 let buttonNo = document.getElementById("buttonNo");
-
-
-
-
 let headers = document.getElementById("headers");
+let body = document.getElementById("body");
+let txtBox = document.getElementById("txtBox");
+let addUserBtn = document.getElementById("addUserBtn");
 
+
+
+
+buttonEn.addEventListener("click",function(evt){
+
+});
+
+buttonNo.addEventListener("click",function(evt){
+
+});
 
 
 
