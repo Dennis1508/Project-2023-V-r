4fa6c55e09aec815cb9916c3397175713830fd0d
LAPTOP-AL2LA1UJ\Dennis An Vo <Dennisanvo@gmail.com>
2023-03-21 13:19:48 +0100

    commit

---
+++ public/index.mjs
@@ -0,0 +1,43 @@
+import Dictionary from "./Dictionary.mjs";
+
+
+let buttonEn = document.getElementById("buttonEn");
+let buttonNo = document.getElementById("buttonNo");
+
+
+
+
+let headers = document.getElementById("headers");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
